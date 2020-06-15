52c11c43cc971fc1c3836787ace4d78936e29543
Christina <christinavhastenrath@gmail.com>
2020-06-15 22:00:09 +0000

    add code in files types: .js,.jsx remove code in files types: .json

---
+++ content.py
@@ -0,0 +1,17 @@
+# Contributions-Importer-For-Github/run_script.py
+
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo("/Users/christina/Desktop/mock-bb/postman-website-www")
+
+# Your mock repo
+mock_repo = git.Repo("/Users/christina/Desktop/mock-repo")
+importer = Importer([repo], mock_repo)
+
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github profiles uses
+# my work email
+importer.set_author(['[christinavhastenrath@gmail.com', 'christina.hastenrath@postman.com'])
+importer.import_repository()
